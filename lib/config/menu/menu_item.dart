2dbbeaf8f06dc70f52356fdf965551446458da69
ApliArte <erbolamm@gmail.com>
2023-07-07 18:18:46 +0200

    progress indicator terminado

--- lib/config/menu/menu_item.dart
+++ lib/config/menu/menu_item.dart
@@ -25,5 +25,11 @@ const appMenuItems = <MenuItem>[
     subTitle: 'Un contenedor estilizado',
     link: '/cards',
     icon: Icons.credit_card_outlined,
-  )
+  ),
+  MenuItem(
+    title: 'ProgressIndicators ',
+    subTitle: 'Generales y Controlados',
+    link: '/progress',
+    icon: Icons.refresh_rounded,
+  ),
 ];
